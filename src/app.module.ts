57a5561a7d6fe3dd5bef5cf4f8336d62fbbb8249
Kush Kaushik <kushkaushik271@gmail.com>
2023-08-19 21:23:08 +0530

    changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { OtpModule } from './otp/otp.module';
+import { ChatModule } from './chat/chat.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, OtpModule, ChatModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
