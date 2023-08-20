7ff48ed982c862614990af28bcb3f5941eb16dbc
Kush Kaushik <kushkaushik271@gmail.com>
2023-08-20 11:26:50 +0530

    new

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import {JwtModule} from '@nestjs/jwt'
 import { jwtSecret } from './jwtSecret';
-
+//changes
 
 @Module({
   imports : [
